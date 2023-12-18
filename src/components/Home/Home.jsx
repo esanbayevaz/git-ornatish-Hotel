88b695cae80b5062e4c64ef33adb1b9449947212
esanbayevaz <esanbayevazebo00@gmail.com>
2023-12-18 19:24:14 +0500

    home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,12 @@ import styles from "./Home.module.css"
 function Home() {
   return (
     <div className={styles.Home}>
+      <form action="">
+        <input type="text" placeholder="isim kiritish" />
+        <input type="text" placeholder="isim kiritish" />
+        <input type="text" placeholder="isim kiritish" />
+
+      </form>
     </div>
   )
 }
